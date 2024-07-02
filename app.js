e0ce2a34880af02e71e5aa734dac0b335537303d
Arthur Smielewski <101211171+axmiel@users.noreply.github.com>
2024-07-02 10:00:03 +0100

    Update app.js

---
+++ app.js
@@ -0,0 +1,8 @@
+/*
+add event listener on the button for post input container
+get text from the input container
+turn that into <p>
+use JS get date method
+turn that into another <p>
+use display flex to align the horizontally, ratio maybe 75/25? 
+*/
