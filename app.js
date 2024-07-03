e6b63c5bcf4b53349a04ea189a5034270e50432d
Arthur Smielewski <101211171+axmiel@users.noreply.github.com>
2024-07-03 01:10:05 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
-/*
-add event listener on the button for post input container
-get text from the input container
-turn that into <p>
-use JS get date method
-turn that into another <p>
-use display flex to align the horizontally, ratio maybe 75/25? 
-*/
+const today = new Date().toDateString().substring(4);
+
+const button = document.getElementById('publish-button').addEventListener('click', function() {
+    //gets value from post input field
+    //creates a new post in the posts container
+    //and clears the value from input field
+    document.querySelector('.posts-inner-container').innerHTML += 
+        '<div class="post">' +
+        '<p class="post-content">' + document.getElementById('post-input').value + '</p>' +
+        '<p class="post-date">' + today.substring(4, 6) + ' ' + today.substring(0, 3) + ' ' + today.substring(7,11) + '</p>';
+    document.getElementById('post-input').value = '';
+})
